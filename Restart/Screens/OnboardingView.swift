1a7489e48caa59139ef2e84a30fb0d70395f440c
Vojin <dvojinovic38@gmail.com>
2022-02-24 16:49:36 +0100

    Levitating animation on second screen and some cool button transition between screens

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -12,18 +12,130 @@ struct OnboardingView: View {
     
     @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
     
+    @State private var buttonWidth: Double = UIScreen.main.bounds.width - 80
+    // CGFloat ce se menjati kako se pomera dugme
+    @State private var buttonOffset: CGFloat = 0
+    // Switcher za animaciju
+    @State private var isAnimating: Bool = false
+    
     //MARK: - BODY
     var body: some View {
-        VStack(spacing: 20) {
-            Text("Onboarding")
-                .font(.largeTitle)
+        ZStack {
+            Color("ColorBlue")
+                .ignoresSafeArea(.all, edges: .all)
             
-            Button(action: {
-                isOnboardingViewActive = false
-            }) {
-                Text("Start")
-            }
-        } //: VSTACK
+            VStack(spacing: 20) {
+                //MARK: - Header
+                
+                Spacer()
+                
+                VStack (spacing: 0) {
+                    Text("Share.")
+                        .font(.system(size: 60))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                    
+                    Text("""
+                        It's not how much we give but how much love we put into giving.
+                    """)
+                        .font(.title3)
+                        .fontWeight(.light)
+                        .foregroundColor(.white)
+                        .multilineTextAlignment(.center)
+                        .padding(.horizontal, 10)
+                }
+                .opacity(isAnimating ? 1 : 0)
+                .offset(y: isAnimating ? 0 : -40)
+                .animation(.easeOut(duration: 1), value: isAnimating)
+                
+                //MARK: - Center
+                
+                ZStack {
+                    CircleGroupView(ShapeColor: .white, ShapeOpacity: 0.2)
+                    
+                    Image("character-1")
+                        .resizable()
+                        .scaledToFit()
+                        .opacity(isAnimating ? 1 : 0)
+                        .animation(.easeOut(duration: 0.5), value: isAnimating)
+                }
+                
+                Spacer() // ovo pushuje header gore
+                
+                //MARK: - FOOTER
+                
+                ZStack {
+                    // 1. BACKGROUND (STATIC)
+                    
+                    Capsule()
+                        .fill(Color.white.opacity(0.2))
+                    Capsule()
+                        .fill(Color.white.opacity(0.2))
+                        .padding(8)
+                    
+                    // 2. CALL-TO-ACTION (STATIC)
+                    
+                    Text("Get started")
+                        .font(.system(.title3, design: .rounded))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .offset(x: 20)
+                    
+                    // 3. CAPSULE (DYNAMIC WIDTH)
+                    
+                    HStack {
+                        Capsule()
+                            .fill(Color("ColorRed"))
+                            .frame(width: buttonOffset + 80)
+                        
+                        Spacer()
+                    }
+                    
+                    // 4. Circle (DRAGGABLE)
+                    HStack {
+                        ZStack {
+                            Circle()
+                                .fill(Color("ColorRed"))
+                            Circle()
+                                .fill(.black.opacity(0.15))
+                                .padding(8)
+                            Image(systemName: "chevron.right.2")
+                                .font(.system(size: 24, weight: .bold))
+                        }
+                        .foregroundColor(.white)
+                    .frame(width: 80, height: 80, alignment: .center)
+                    .offset(x: buttonOffset)
+                    .gesture(
+                        DragGesture()
+                            .onChanged { gesture in
+                                if gesture.translation.width > 0 && buttonOffset <= buttonWidth - 80 {
+                                    buttonOffset = gesture.translation.width
+                                }
+                            }
+                            .onEnded { _ in
+                                withAnimation(Animation.easeOut(duration: 0.65)) {
+                                    if buttonOffset > buttonWidth / 2 {
+                                        buttonOffset = buttonWidth - 80
+                                        isOnboardingViewActive = false
+                                    } else {
+                                        buttonOffset = 0
+                                    }
+                                }
+                            }
+                    )
+                        Spacer()
+                    }
+                }
+                .frame(width: buttonWidth, height: 80, alignment: .center)
+                .padding()
+                .opacity(isAnimating ? 1 : 0)
+                .offset(y: isAnimating ? 0 : 40)
+                .animation(.easeOut(duration: 1), value: isAnimating)
+            } //: VSTACK
+        } //: ZSTACK
+        .onAppear(perform:  {
+            isAnimating = true
+        })
     }
 }
 
