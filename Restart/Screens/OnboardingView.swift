5d1c1327e179191aec4c4e683684c9df8b00a99c
Vojin <dvojinovic38@gmail.com>
2022-02-24 17:26:29 +0100

    Main view cool animation add with parallax effect.

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -17,6 +17,11 @@ struct OnboardingView: View {
     @State private var buttonOffset: CGFloat = 0
     // Switcher za animaciju
     @State private var isAnimating: Bool = false
+    // Kada se zapocne initial verzije ce biti 0
+    //@State private var imageOffset: CGSize = CGSize(width: 0, height: 0) same thing, less code
+    @State private var imageOffset: CGSize = .zero
+    @State private var indicatorOpacity: Double = 1.0
+    @State private var textTitle: String = "Share."
     
     //MARK: - BODY
     var body: some View {
@@ -30,10 +35,12 @@ struct OnboardingView: View {
                 Spacer()
                 
                 VStack (spacing: 0) {
-                    Text("Share.")
+                    Text(textTitle)
                         .font(.system(size: 60))
                         .fontWeight(.heavy)
                         .foregroundColor(.white)
+                        .transition(.opacity)
+                        .id(textTitle)
                     
                     Text("""
                         It's not how much we give but how much love we put into giving.
@@ -52,13 +59,51 @@ struct OnboardingView: View {
                 
                 ZStack {
                     CircleGroupView(ShapeColor: .white, ShapeOpacity: 0.2)
+                        .offset(x: imageOffset.width * -1)
+                        .blur(radius: abs(imageOffset.width / 5))
+                        .animation(.easeOut(duration: 1), value: imageOffset)
                     
                     Image("character-1")
                         .resizable()
                         .scaledToFit()
                         .opacity(isAnimating ? 1 : 0)
                         .animation(.easeOut(duration: 0.5), value: isAnimating)
+                        .offset(x: imageOffset.width * 1.2, y: 0)
+                        .rotationEffect(.degrees(Double(imageOffset.width / 15)))
+                        .gesture(
+                            DragGesture()
+                                .onChanged { gesture in
+                                    // abs uproscava cinjenicu da user moze da vuce desno i levo, apsolutni broj ce uvek biti pozitivan
+                                    if abs(imageOffset.width) <= 150 {
+                                        imageOffset = gesture.translation
+                                        
+                                        withAnimation(.linear(duration: 0.25)) {
+                                            indicatorOpacity = 0
+                                            textTitle = "Give."
+                                        }
+                                    }
+                                }
+                                .onEnded { _ in
+                                    imageOffset = .zero
+                                    
+                                    withAnimation(.linear(duration: 0.25)) {
+                                        indicatorOpacity = 1
+                                        textTitle = "Share."
+                                    }
+                                }
+                        )
+                        .animation(.easeOut(duration: 1), value: imageOffset)
                 }
+                .overlay(
+                    Image(systemName: "arrow.left.and.right.circle")
+                        .font(.system(size: 44, weight: .ultraLight))
+                        .foregroundColor(.white)
+                        .offset(y: 20)
+                        .opacity(isAnimating ? 1 : 0)
+                        .animation(.easeOut(duration: 1).delay(2), value: isAnimating)
+                        .opacity(indicatorOpacity)
+                    , alignment: .bottom
+                )
                 
                 Spacer() // ovo pushuje header gore
                 
